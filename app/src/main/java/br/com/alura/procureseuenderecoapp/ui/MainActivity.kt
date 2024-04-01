d62c429248641c5a61e550171c147040ed36379f
Victor Torres <vhugosoria2@gmail.com>
2024-04-01 11:33:50 -0300

    Configurando o projeto para receber o retrofit no layout

--- app/src/main/java/br/com/alura/procureseuenderecoapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/alura/procureseuenderecoapp/ui/MainActivity.kt
@@ -2,11 +2,57 @@ package br.com.alura.procureseuenderecoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.alura.procureseuenderecoapp.R
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.procureseuenderecoapp.data.repository.AddressRepository
+import br.com.alura.procureseuenderecoapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        tryFindCep()
+
+
     }
+
+
+    fun tryFindCep(){
+        binding.mainButtonSearch.setOnClickListener {
+            val cep = binding.mainTextFieldCepInput.text.toString()
+            lifecycleScope.launch {
+                try {
+                    AddressRepository().findAddress(cep).let{ adress ->
+                        binding.apply {
+                            mainTextFieldCepInput.setText(adress.cep)
+                            mainTextFieldInputLogradouro.setText(adress.logradouro)
+                            mainTextFieldInputComplemento.setText(adress.complemento)
+                            mainTextFieldInputBairro.setText(adress.bairro)
+                            mainTextFieldInputLocalidade.setText(adress.localidade)
+                            mainTextFieldInputUf.setText(adress.uf)
+                        }
+
+                    }
+
+                }catch (e: Exception){
+                    Log.e("Erro", "${e.message}")
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle("Tente Novamente")
+                        .setMessage("Não conseguimos achar o cep")
+                        .setPositiveButton("Continue"){ _, _ ->   }
+                        .show()
+                }
+            }
+        }
+
+
+    }
+
 }
