2060a109e0e1ac53ccbf561f84f660e33e0c470c
Victor Torres <vhugosoria2@gmail.com>
2024-04-01 11:27:02 -0300

    Criando o layout

--- app/src/main/java/br/com/alura/procureseuenderecoapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/alura/procureseuenderecoapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.alura.procureseuenderecoapp
+package br.com.alura.procureseuenderecoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.alura.procureseuenderecoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
